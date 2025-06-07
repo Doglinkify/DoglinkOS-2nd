ba868bad36ff10f1f4b855ad44d301a89eeb9b09
Yukai Wu <wuyukai0403@qq.com>
2025-06-07 19:32:48 +0800

    make hello_std optional

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -83,6 +83,7 @@ fn main() {
 fn build_img() -> PathBuf {
     let doglinked_path = Path::new(env!("CARGO_BIN_FILE_DOGLINKED"));
     let t_path = Path::new(env!("CARGO_BIN_FILE_INFINITE_LOOP"));
+    #[cfg(feature = "dlos_std")]
     let helo_path = Path::new(env!("CARGO_BIN_FILE_HELLO_STD"));
 
     let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
@@ -91,6 +92,7 @@ fn build_img() -> PathBuf {
     let initrd_files = BTreeMap::from([
         ("doglinked", doglinked_path.to_path_buf()),
         ("exiter", t_path.to_path_buf()),
+        #[cfg(feature = "dlos_std")]
         ("hello_std", helo_path.to_path_buf()),
     ]);
     let initrd_path = manifest_dir.parent().unwrap().join("initrd.img");

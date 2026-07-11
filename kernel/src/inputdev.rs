bf68367069c8a3585469185f226984ce3d57fd28
Yukai Wu <wuyukai0403@qq.com>
2026-07-11 15:22:19 +0800

    make clippy happy

--- kernel/src/inputdev.rs
+++ kernel/src/inputdev.rs
@@ -40,12 +40,14 @@ fn read_data() -> Result<u8, Error> {
 
 fn write_command(cmd: u8) -> Result<(), Error> {
     wait_write()?;
-    unsafe { Ok(PortWriteOnly::new(0x64).write(cmd)) }
+    unsafe { PortWriteOnly::new(0x64).write(cmd);
+    Ok(()) }
 }
 
 fn write_data(cmd: u8) -> Result<(), Error> {
     wait_write()?;
-    unsafe { Ok(PortWriteOnly::new(0x60).write(cmd)) }
+    unsafe { PortWriteOnly::new(0x60).write(cmd);
+    Ok(()) }
 }
 
 fn flush_output() {
